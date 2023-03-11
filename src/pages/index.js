d1af5cf997ecedc947330fb764aefea20dd1849d
mnedzhib <mesut.nedzhib@gmail.com>
2023-03-11 15:38:39 +0200

    Add latest files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div>
       <Head>
@@ -11,7 +13,27 @@ export default function Home() {
 
       <Header />
 
-      <Footer />
+      <main className="max-w-screen-2xl mx-auto bg-gray-100 ">
+        {/* Banner */}
+        <Banner />
+
+        {/* Product feeds */}
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products: products,
+    },
+  };
+}
+
+// Get -> https://fakestoreapi.com/products
